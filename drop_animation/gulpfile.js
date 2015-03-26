30fe3a8cb0b72cd13638b50ab6af0c7911f1057a
林 佳代子 <hayashi_kayoko@iMac1303260402-2.local>
2015-03-26 17:37:09 +0900

    桜と雪のアニメーション

---
+++ drop_animation/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+// var uglify = require("gulp-uglify");
+
+gulp.task("js", function() {
+    gulp.src(["js/**/*.js","!js/min/**/*.js"])
+        .pipe(plumber())
+        .pipe(frontnote({
+            css: '../css/style.css'
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task("sass", function() {
+    gulp.src("sass/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"));
+});
+
+gulp.task("default",['server'], function() {
+    gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+    gulp.watch("sass/**/*.scss",["sass"]);
+});
