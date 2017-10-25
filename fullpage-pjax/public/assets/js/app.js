40c6b7764561e9c87c2657116ce0c00294001495
林 佳代子 <hayashi@liginc.co.jp>
2017-10-25 16:02:57 +0900

    DEMO完成

--- fullpage-pjax/public/assets/js/app.js
+++ fullpage-pjax/public/assets/js/app.js
@@ -1,1 +1,167 @@
-!function(e){function __webpack_require__(_){if(r[_])return r[_].exports;var t=r[_]={i:_,l:!1,exports:{}};return e[_].call(t.exports,t,t.exports,__webpack_require__),t.l=!0,t.exports}var r={};return __webpack_require__.m=e,__webpack_require__.c=r,__webpack_require__.i=function(e){return e},__webpack_require__.d=function(e,r,_){Object.defineProperty(e,r,{configurable:!1,enumerable:!0,get:_})},__webpack_require__.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return __webpack_require__.d(r,"a",r),r},__webpack_require__.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},__webpack_require__.p="/assets/js/",__webpack_require__(__webpack_require__.s=0)}([function(e,r){"use strict";var _=$("#js-fullpage"),t=$(".btn");_.fullpage({easing:"easeOut",scrollingSpeed:1e3}),t.on("click",function(e){e.preventDefault();var r=$(e.currentTarget).data("page");$.fn.fullpage.moveTo(r)}),$.pjax({areas:["#js-pjax"],link:"a.pjax",update:{head:"base, meta",css:!1,script:!1},wait:3e3,reset:{type:"click progress itemload allload enter leave scroll mousemove touchstart touchmove resize"}})}]);
+console.warn("This script is development version.");
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmory imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmory exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		Object.defineProperty(exports, name, {
+/******/ 			configurable: false,
+/******/ 			enumerable: true,
+/******/ 			get: getter
+/******/ 		});
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/assets/js/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+"use strict";
+'use strict';
+
+/**
+ * @constructor
+ */
+var $window = $(window);
+var $cover = $('.page-cover');
+
+/*-------
+ fullpage.js
+ */
+
+function fullPage() {
+    var $target = $('#js-fullpage');
+    var $btn = $('.btn');
+
+    console.log('fullpage');
+    //fullpage.jsの設定
+    $target.fullpage({
+        easing: 'easeOut',
+        scrollingSpeed: 1000,
+        lockAnchors: true,
+        animateAnchor: false
+    });
+
+    //ページ送りボタン
+    $btn.on('click.fullpage', function (e) {
+        e.preventDefault();
+        var movePage = $(e.currentTarget).data('page');
+        $.fn.fullpage.moveTo(movePage);
+    });
+}
+
+/*-------
+ 初期設定
+ */
+$window.on('load', function () {
+    //幕を開ける
+    $('.page-cover').addClass('is-up');
+
+    //fullpage.js
+    fullPage();
+});
+
+/*-------
+ Pjaxの操作
+ */
+$.pjax({
+    areas: ['#js-pjax'], //再描画されるエリア
+    link: 'a.pjax', //pjaxを展開させるlinkになるクラス
+    update: { head: 'base, meta', css: false, script: false }, //ページ更新したい箇所の設定
+    wait: 3000, //押してからURLが変わるまでの時間
+    reset: { //イベントのリセット
+        type: 'click progress itemload allload enter leave scroll mousemove touchstart touchmove resize'
+    }
+});
+
+/*-------
+ Pjax実行前
+ */
+$(document).on('pjax:fetch', function () {
+    console.log('実行前');
+    //幕を閉じる
+    $('.page-cover').removeClass('is-up');
+
+    //indexから離れる時
+    if ($('#js-fullpage').length) {
+
+        setTimeout(function () {
+            console.log('解除');
+            $.fn.fullpage.destroy('all');
+        }, 1000);
+    }
+});
+
+/*-------
+ Pjax実行後
+ */
+$(document).on('pjax:ready', function () {
+    console.log('実行後');
+
+    setTimeout(function () {
+        //幕を開ける
+        $('.page-cover').addClass('is-up');
+    }, 2000);
+
+    //indexに到達した時
+    if ($('#js-fullpage').length) {
+        fullPage();
+    }
+});
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=maps/app.map
--- fullpage-pjax/src/js/app.js
+++ fullpage-pjax/src/js/app.js
@@ -1,36 +1,91 @@
 /**
  * @constructor
  */
-const $target = $('#js-fullpage');
-const $btn = $('.btn');
-
+const $window = $(window);
+const $cover = $('.page-cover');
 
 /*-------
- Fullpage.js
+ fullpage.js
  */
 
-$target.fullpage({
-  easing: 'easeOut',
-  scrollingSpeed: 1000
+function fullPage(){
+    const $target = $('#js-fullpage');
+    const $btn = $('.btn');
+
+    console.log('fullpage');
+    //fullpage.jsの設定
+    $target.fullpage({
+        easing: 'easeOut',
+        scrollingSpeed: 1000,
+        lockAnchors: true,
+        animateAnchor: false
+    });
+
+    //ページ送りボタン
+    $btn.on('click.fullpage',(e)=>{
+        e.preventDefault();
+        let movePage = $(e.currentTarget).data('page');
+        $.fn.fullpage.moveTo(movePage);
+    });
+}
+
+/*-------
+ 初期設定
+ */
+$window.on('load',()=>{
+    //幕を開ける
+    $('.page-cover').addClass('is-up');
+
+    //fullpage.js
+    fullPage();
 });
 
-//ページ送りボタン
-$btn.on('click',(e)=>{
-  e.preventDefault();
-let movePage = $(e.currentTarget).data('page');
-$.fn.fullpage.moveTo(movePage);
-});
+
 
 /*-------
  Pjaxの操作
  */
 $.pjax({
-  areas:['#js-pjax'],//DOM
-  link:'a.pjax',
-  update: {head: 'base, meta', css: false, script: false},
-  wait:3000,
-  reset: {
+  areas:['#js-pjax'],//再描画されるエリア
+  link:'a.pjax',//pjaxを展開させるlinkになるクラス
+  update: {head: 'base, meta', css: false, script: false},//ページ更新したい箇所の設定
+  wait:3000,//押してからURLが変わるまでの時間
+  reset: { //イベントのリセット
     type: 'click progress itemload allload enter leave scroll mousemove touchstart touchmove resize'
   }
 });
 
+/*-------
+ Pjax実行前
+ */
+$(document).on('pjax:fetch',function() {
+
+    //幕を閉じる
+    $('.page-cover').removeClass('is-up');
+
+    //indexから離れる時
+    if($('#js-fullpage').length){
+
+        setTimeout(function(){
+            console.log('解除');
+            $.fn.fullpage.destroy('all');
+        },1000);
+    }
+
+});
+
+/*-------
+ Pjax実行後
+ */
+$(document).on('pjax:ready',function() {
+
+    setTimeout(()=>{
+        //幕を開ける
+        $('.page-cover').addClass('is-up');
+    },2000);
+
+    //indexに到達した時
+    if($('#js-fullpage').length){
+        fullPage();
+    }
+});
